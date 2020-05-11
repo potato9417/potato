6b4dfa58df527e3ecaad5921d1f468c6e109e51a
potato9417 <potato9417@gmail.com>
2020-05-11 14:11:41 +0900

    20200511- 웨않뒈

--- onlyJS/Filter-Project-master/js/app.js
+++
@@ -1,6 +0,0 @@
-const btnAll = document.querySelector("#store .row .btnAll"),
-    btnCakes = document.querySelector("#store .row .btnCakes"),
-    btnCupcajes = document.querySelector("#store .row .btnCupcajes"),
-    btnSweets = document.querySelector("#store .row .btnSweets"),
-    btnDoughnuts = document.querySelector("#store .row .btnDoughnuts");
-
--- onlyJS/Filter-Project-master/js/index.js
+++ onlyJS/Filter-Project-master/js/index.js
@@ -8,7 +8,9 @@ const  categoryAll = document.getElementById("all"),
     itemList = document.querySelectorAll("#list ul li"),
     inputItem = document.getElementById("inputItem");
 
-let num = 0;
+let num = 0,
+    itemLetterArr=[],
+    inputLetterArr=[];
 
 
 
@@ -64,29 +66,40 @@ function searchItemEvent(){
     itemList.forEach(function(e){
         // console.log(e.outerText);
         // console.log(e);
-        e.classList.remove("hidden");
+        e.classList.add("hidden");
         let itemName = e.getElementsByClassName("name");
         // console.log(itemName[0].innerHTML.toUpperCase().split(""));
         let splitArr = itemName[0].innerHTML.toUpperCase().split("");
         let splitInputArr = inputItem.value.toUpperCase().split("");
 
+        // 배열을 문자로 바꿔둠
         let splitStringArr = splitArr.toString();
         let splitStringInputArr = splitInputArr.toString();
 
         // 지금 입력한 단어
+        let itemLetter = splitArr[num];
         let inputLetter = splitInputArr[num];
 
-        // 정규식.. 웨않뒈?
-        const regex = /[^\w\s]/splitInputArr[num];
+        // // 정규식.. 웨않뒈?
+        // const regex = /[inputLetter]/g;
+        // console.log(itemLetter.search(regex));
         
-        console.log(splitInputArr[num])
-        // console.log(splitStringArr,"==",splitStringInputArr)
+        
+        console.log(itemLetter,"==",inputLetter)
+        // console.log(window)
 
-        // 입력한 글자와 이름 앞글자가 같을 경우 두번쨰글자부터 인식못함..
-        if(splitArr.lastIndexOf(splitStringInputArr)){
-            console.log("입력인식")
-            e.classList.add("hidden")
-        };
+        // 입력한 글자의 위치의 글자와 입력한 글자가 같을떄.. cake을 입력할떄 e를 입력할떄  sweet의 e가 인식되벌임.. 싯...ㅍ..ㅏ..
+        
+        if(itemLetter==inputLetter){
+            e.classList.remove("hidden");
+        }
+        
+
+        // // 입력한 글자와 이름 앞글자가 같을 경우 두번쨰글자부터 인식못함..
+        // if(splitArr.lastIndexOf(splitStringInputArr)){
+        //     console.log("입력인식")
+        //     e.classList.add("hidden")
+        // };
 
         
         // // 입력한 글자와 이름이 모두 같을 경우
